72896f185d9899f26cf10460686ad0aaaa512e50
Jorge <jocady21@gmail.com>
2023-07-19 02:24:56 -0300

    a
    cambios

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,46 @@
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
 
+// español
+let who1 = ['El perro', 'Mi abuela', 'Su tortuga', 'Mi pájaro'];
+let action1 = ['comió', 'meó', 'aplastó', 'rompió'];
+let what1 = ['mi tarea', 'las llaves', 'el carro'];
+let when1 = ['antes de la clase', 'justo a tiempo', 'cuando terminé', 'durante mi almuerzo', 'mientras oraba'];
+
+//ingles
+let who2 = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action2 = ['ate', 'peed', 'crushed', 'broke'];
+let what2 = ['my homework', 'the keys', 'the car'];
+let when2 = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+//portuguez
+let who3 = ['O cachorro', 'Minha avó', 'Sua tartaruga', 'Meu pássaro'];
+let action3 = ['comeu', 'fez xixi', 'esmagou', 'quebrou'];
+let what3 = ['meu dever de casa', 'as chaves', 'o carro'];
+let when3 = ['antes da aula', 'bem na hora', 'quando terminei', 'durante meu almoço', 'enquanto eu estava orando'];
+
+//japones
+let who4 = ['inu','watashi no sobo','kare no kame','watashi no tori'];
+let action4 = [' tabeta',' oshikko shita',' oshitsubusa reta',' kowareta']; 
+let what4 = ['watashi no shukudai','kagi','-sha']; 
+let when4 = ['jugyō no mae ni','jikandōrini','jugyo ga owatta toki','chūshoku-chū','inotte iru ma'];
+
+//aleman
+let who5 = ['Der Hund', 'Meine Oma', 'Seine Schildkröte', 'Mein Vogel'];
+let action5 = ['ate', 'pinkel', 'crushed', 'broke'];
+let what5 = ['meine Hausaufgaben', 'die Schlüssel', 'das Auto'];
+let when5 = ['vor dem Unterricht', 'pünktlich', 'als ich fertig war', 'während meines Mittagessens', 'während ich betete'];
+
+
+
+
+//logica
+let idioma = 0
+
+if (condition) {
+
+    idioma = 1 
+
+    
+}
 
 
 
