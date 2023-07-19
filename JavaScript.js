68e33d0cf3ef8bbd758361a53f4cf91a7cb5e7d8
Jorge <jocady21@gmail.com>
2023-07-19 14:57:49 -0300

    Generador de escusas
    cambios

--- JavaScript.js
+++ JavaScript.js
@@ -16,6 +16,7 @@ const lengujues = {
 
     Ingles:{
         
+        who    :['The dog', 'My grandma', 'His turtle', 'My bird'],
         action :['ate', 'peed', 'crushed', 'broke'],
         what   :['my homework', 'the keys', 'the car'],
         when   :['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'],
