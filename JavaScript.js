af175c460f2d47ef9121e599179bc36fdadc4d7d
Jorge <jocady21@gmail.com>
2023-07-19 20:35:21 -0300

    Generador de Excusas
    Commit Final

--- JavaScript.js
+++ JavaScript.js
@@ -13,7 +13,7 @@ let when2 = ['before the class', 'right on time', 'when I finished', 'during my 
 
 //Objetos
 const lengujues = {
-
+//objeto arrays
     Ingles:{
         
         who    :['The dog', 'My grandma', 'His turtle', 'My bird'],
@@ -22,7 +22,7 @@ const lengujues = {
         when   :['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'],
 
     },
-
+//objeto arrays
     Español:{
         
         quien  :['El perro', 'Mi abuela', 'Su tortuga', 'Mi pájaro'],
@@ -34,12 +34,11 @@ const lengujues = {
 }
 
 const Pagina_español = document.getElementById("es")
-//Funcion para vambiar el valor del value del boton "boton" a "Español"
+//Funcion para cambiar el valor del value del boton "boton" a "Español"
 function es(){
 
     let valor = document.getElementById("boton")
     valor.value = "Español"
-    console.log("Español")
 
 }
 
@@ -50,45 +49,35 @@ function en(){
 
     let valor = document.getElementById("boton")
     valor.value = "Ingles"
-    console.log("Ingles")
-    let titulo = document.g
-
-    
-
+   
 }
 
-
-
-//logica
+//funcion para crear la escusa en el idioma elejido
 function cambiar (){
 
     const butom_idioma = document.getElementById ("boton")
     console.log(butom_idioma.value)
+    //si el idioma es español se realizara la siguiente logica:
     if (butom_idioma.value==="Ingles") {
         console.log("es inglesh")
-
+        //math.random cra un numero al azar y Math.floor lo redondea a un entero
         let whonr    = Math.floor(Math.random()*(4 - 0 )+ 0);
         let actionnr = Math.floor(Math.random()*(4 - 0 )+ 0);
         let whatnr   = Math.floor(Math.random()*(3 - 0 )+ 0);
         let whennr   = Math.floor(Math.random()*(5 - 0 )+ 0);
-
+        //se concatena el mensaje y se almacena en la variable
         let escusa =  lengujues.Ingles.who[whonr] + " " + lengujues.Ingles.action[actionnr] + " " + lengujues.Ingles.what[whatnr] + " " + lengujues.Ingles.when[whennr]  
-        console.log(escusa)
-        console.log(whonr)
         return escusa;
-
+    //si el idioma es español se ejecutara la siguiente logica:
     } else if(butom_idioma.value==="Español"){
-        console.log("es españolsh")
-
-
+       
         let quiennr  = Math.floor(Math.random()*(4 - 0 )+ 0);
         let accionnr = Math.floor(Math.random()*(4 - 0 )+ 0);
         let quenr    = Math.floor(Math.random()*(3 - 0 )+ 0);
         let cuandonr = Math.floor(Math.random()*(5 - 0 )+ 0);
 
         let escusa =  lengujues.Español.quien[quiennr] + " " + lengujues.Español.accion[accionnr] + " " + lengujues.Español.que[quenr] + " " + lengujues.Español.cuando[cuandonr]   
-        console.log(escusa)
-        console.log(quiennr)
+
         return escusa;
     }
 
@@ -97,29 +86,15 @@ function cambiar (){
 
 
 
-
-
 const boton = document.getElementById("boton")
-
+//funcion encargada de carcar la escusa en el parrafo "escus"
 function render (){
     console.log ("este sech")
-    const resultado = cambiar()
-    
-    const parrafo_resultado = document.getElementById("escus")
-    
+    const resultado = cambiar() 
     return document.getElementById("escus").innerHTML = resultado;
-    
-        
     
     
 }
-
-
-
-
-
-    
-
 
 
 
